3b73a87eb1e10c913b6b20bfabab0bd44c6b0116
milonite <milonite@hotmail.it>
2025-01-20 10:31:32 +0100

    wip

--- src/helpers.ts
+++ src/helpers.ts
@@ -46,6 +46,7 @@ import { InfraredHoneyUsdcShortcut } from './shortcuts/infrared/honey-usdc';
 import { InfraredWethHoneyShortcut } from './shortcuts/infrared/weth-honey';
 import { InfraredWethWbtcShortcut } from './shortcuts/infrared/weth-wbtc';
 import { KodiakHoneyUsdcShortcut } from './shortcuts/kodiak/honey-usdc';
+import { KodiakUsdtHoneyShortcut } from './shortcuts/kodiak/usdt-honey';
 import { KodiakWethHoneyShortcut } from './shortcuts/kodiak/weth-honey';
 import { KodiakWethWbtcShortcut } from './shortcuts/kodiak/weth-wbtc';
 import { OrigamiBoycoHoneyShortcut } from './shortcuts/origami/oboy-HONEY-a';
@@ -106,6 +107,7 @@ export const shortcuts: Record<string, Record<string, Shortcut>> = {
     'honey-usdc': new KodiakHoneyUsdcShortcut(),
     'weth-honey': new KodiakWethHoneyShortcut(),
     'weth-wbtc': new KodiakWethWbtcShortcut(),
+    'usdt-honey': new KodiakUsdtHoneyShortcut(),
   },
   origami: {
     'oboy-honey': new OrigamiBoycoHoneyShortcut(),
