7de902d24e8b28f7ef6d3a42005f6dc9fd2dc30e
milonite <milonite@hotmail.it>
2025-01-26 20:38:38 +0100

    add d2

---
+++ src/shortcuts/d2/usdc-kodiakplus.ts
@@ -0,0 +1,75 @@
+import { Builder } from '@ensofinance/shortcuts-builder';
+import { RoycoClient } from '@ensofinance/shortcuts-builder/client/implementations/roycoClient';
+import { AddressArg, ChainIds, WeirollScript } from '@ensofinance/shortcuts-builder/types';
+import { TokenAddresses } from '@ensofinance/shortcuts-standards/addresses';
+
+import { chainIdToDeFiAddresses, chainIdToTokenHolder } from '../../constants';
+import type { AddressData, Input, Output, Shortcut } from '../../types';
+import { ensureMinAmountOut, getBalance, mintErc4626 } from '../../utils';
+
+export class D2UsdcKodiakplusShortcut implements Shortcut {
+  name = 'usdc-kodiakplus';
+  description = '';
+  supportedChains = [ChainIds.Cartio, ChainIds.Berachain];
+  inputs: Record<number, Input> = {
+    [ChainIds.Cartio]: {
+      usdc: TokenAddresses.cartio.usdc,
+      vault: '0xa4869CbdC3Bc1B71b7C29e642207bb9439Ac05ba',
+    },
+    [ChainIds.Berachain]: {
+      usdc: chainIdToDeFiAddresses[ChainIds.Berachain].usdc,
+      vault: '0x2B8D0420996a2753EF21C25C94eAe9fc0c0Aed1E',
+    },
+  };
+  setterInputs = new Set(['minAmountOut']);
+
+  async build(chainId: number): Promise<Output> {
+    const client = new RoycoClient();
+
+    const inputs = this.inputs[chainId];
+    const { usdc, vault } = inputs;
+
+    const builder = new Builder(chainId, client, {
+      tokensIn: [usdc],
+      tokensOut: [vault],
+    });
+
+    const usdcAmount = getBalance(usdc, builder);
+
+    const vaultAmount = await mintErc4626(usdc, vault, usdcAmount, builder);
+    ensureMinAmountOut(vaultAmount, builder);
+
+    const payload = await builder.build({
+      requireWeiroll: true,
+      returnWeirollScript: true,
+    });
+
+    return {
+      script: payload.shortcut as WeirollScript,
+      metadata: builder.metadata,
+    };
+  }
+
+  getAddressData(chainId: number): Map<AddressArg, AddressData> {
+    switch (chainId) {
+      case ChainIds.Cartio:
+        return new Map([
+          [this.inputs[ChainIds.Cartio].usdc, { label: 'ERC20:USDC' }],
+          [this.inputs[ChainIds.Cartio].vault, { label: 'ERC20:D2 Vault' }],
+        ]);
+      case ChainIds.Berachain:
+        return new Map([
+          [this.inputs[ChainIds.Berachain].usdc, { label: 'ERC20:USDC' }],
+          [this.inputs[ChainIds.Berachain].vault, { label: 'ERC20:D2 Vault' }],
+        ]);
+      default:
+        throw new Error(`Unsupported chainId: ${chainId}`);
+    }
+  }
+  getTokenHolder(chainId: number): Map<AddressArg, AddressArg> {
+    const tokenToHolder = chainIdToTokenHolder.get(chainId);
+    if (!tokenToHolder) throw new Error(`Unsupported 'chainId': ${chainId}`);
+
+    return tokenToHolder as Map<AddressArg, AddressArg>;
+  }
+}
